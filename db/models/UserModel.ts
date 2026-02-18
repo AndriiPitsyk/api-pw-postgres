6d9ebfe68a1cceb96edb1e1a10c57564e10c168a
a.pitsyk <andrii.pitsyk@persgroep.net>
2026-02-18 22:36:38 +0200

    refactor database connection logic

--- db/models/UserModel.ts
+++ db/models/UserModel.ts
@@ -1,24 +1,20 @@
-import { ClientConnection } from '../connections/client';
 import { Client } from 'pg';
 
 export class UserModel {
-    private client: Client;
-
-    constructor(private connection: ClientConnection) {
-        this.client = this.connection.gitClientInstance();
+    constructor(private client: Client) {
     }
 
-    async getUserByEmail(email: string): Promise<object> {
+    async getUserByEmail(email: string): Promise<any> {
         const result = await this.client.query(`SELECT * FROM users WHERE email = $1`, [email]);
         return result.rows[0];
     }
 
-    async getUserById(id: number): Promise<object> {
+    async getUserById(id: number): Promise<any> {
         const result = await this.client.query(`SELECT * FROM users WHERE id = $1`, [id]);
         return result.rows[0];
     }
 
-    async deleteByEmail(email: string) {
+    async deleteByEmail(email: string): Promise<void> {
         await this.client.query(`DELETE FROM users WHERE email = $1`, [email]);
     }
 }
