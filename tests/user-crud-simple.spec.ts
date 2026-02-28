8537865b0efac57eb58a7b3aa0835f83377f728d
a.pitsyk <andrii.pitsyk@persgroep.net>
2026-02-28 16:08:25 +0200

    extended simple crud api tests

--- tests/user-crud-simple.spec.ts
+++ tests/user-crud-simple.spec.ts
@@ -13,6 +13,19 @@ test('Should successfully create a new user', async ({api}) => {
     });
 })
 
+test('Should successfully get a new user data', async ({api}) => {
+    const testUser = userData.generate();
+    user = await api.users.create(testUser);
+    const getUserData = await api.users.getUserByEmail(user.email);
+    expect(getUserData, 'User data is invalid').toMatchObject({
+        email: testUser.email,
+        userName: testUser.userName,
+        id: expect.any(Number),
+        updatedAt: expect.any(String),
+        createdAt: expect.any(String)
+    });
+})
+
 test('Should successfully update user', async ({api}) => {
     const [userOne, userTwo] = [userData.generate(), userData.generate()];
     const response = await api.users.create(userOne);
